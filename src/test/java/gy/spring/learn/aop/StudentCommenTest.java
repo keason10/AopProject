d97a1a8f49af5697dd205aca4e7f9d836d6dd8a9
yantao <yantao@guanyisoft.com>
2017-04-03 22:19:06 +0800

    提交test ，咋在test中，runWith /ContextConfiguration @autowrired

--- src/test/java/gy/spring/learn/aop/StudentCommenTest.java
+++ src/test/java/gy/spring/learn/aop/StudentCommenTest.java
@@ -3,7 +3,6 @@ package gy.spring.learn.aop;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,9 +12,16 @@ import static org.testng.Assert.*;
 /**
  * Created by yantao on 2017/4/3.
  */
-@RunWith(value = SpringJUnit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
 public class StudentCommenTest extends AbstractJUnit4SpringContextTests {
+
+    /**
+     * 开始报错，这是由于spring-test jar 和srping 的版本不兼容问题，要用相同的版本
+     * java.lang.IllegalStateException: Could not load TestContextBootstrapper [null]. Specify @BootstrapWith's 'value' attribute or
+     * make the default bootstrapper class available.
+
+     */
     @Autowired
     StudentCommen studentCommen;
 
