7e7672da788054a60da1fcdd776a94dd061cbc72
yantao <tao.yan@picahealth.com>
2018-03-11 19:19:51 +0800

    ddd

--- src/main/java/gy/spring/learn/utils/TimeUtils.java
+++ src/main/java/gy/spring/learn/utils/TimeUtils.java
@@ -1,7 +1,9 @@
 package gy.spring.learn.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by yantao on 2017/4/15.
@@ -10,4 +12,10 @@ public class TimeUtils {
     public static void showTime() {
         System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
+
+    public static void main(String[] args) {
+        System.out.println(1==1.00);
+    }
+
+
 }
